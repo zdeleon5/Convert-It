49f22f72be48365b9c7e0e30d94f340564cd3021
Zinedine De Leon <108776201+zdeleon5@users.noreply.github.com>
2024-08-17 19:41:00 -0500

    Add files via upload

---
+++ Convert_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Convert_ItApp.swift
+//  Convert It
+//
+//  Created by Zinedine De Leon on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Convert_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
